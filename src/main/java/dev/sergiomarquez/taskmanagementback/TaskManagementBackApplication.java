097b464f5984abb2fb381bee8b04a2105b79826c
Sergio Márquez <marquezpsergio@gmail.com>
2024-07-13 16:56:40 +0200

    Initial commit

---
+++ src/main/java/dev/sergiomarquez/taskmanagementback/TaskManagementBackApplication.java
@@ -0,0 +1,13 @@
+package dev.sergiomarquez.taskmanagementback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagementBackApplication.class, args);
+    }
+
+}
